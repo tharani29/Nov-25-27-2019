a7f9bf0217128f40b17ce4d04ba3c33a1cb1a721
prabhu-durasoft <iamprabhu@yahoo.com>
2019-11-26 10:03:08 +0530

    Day02

---
+++ Day02/strings.js
@@ -0,0 +1,12 @@
+//Template strings
+let name = "Sam";
+//let message = "Hi " + name;
+
+let message = `Hi ${name}`;
+console.log(message);
+
+let para = `
+	hello!
+	what's up?
+`
+console.log(para);
